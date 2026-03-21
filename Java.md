c407493df75706cb15e8f47d8134caa05c91fe5b
menjinyan <menjin728@gmail.com>
2026-03-22 00:37:26 +0800

    多线程的第三种启动方式(Callable)

--- Java.md
+++ Java.md
@@ -267,4 +267,6 @@ Stream 接口中静态方法 of 的细节
 ### 并发和并行  
 ![img.png](document.image/并发和并行.png)  
 ### 多线程的实现方式  
-![img.png](document.image/多线程的实现方式.png)
+![img.png](document.image/多线程的实现方式.png)  
+### 多线程三种方式对比  
+![img.png](document.image/多线程三种方式对比.png)
---
+++ src/A03Thread/MyCallable.java
@@ -0,0 +1,16 @@
+package A03Thread;
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+
+        //求1~100之间的和
+        int sum=0;
+        for (int i = 0; i <= 100; i++) {
+            sum=sum+i;
+        }
+        return sum;
+    }
+}
