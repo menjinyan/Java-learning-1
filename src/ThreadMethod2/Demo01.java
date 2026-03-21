30c357a1673cf07f427e1b8e7464335fd005a067
menjinyan <menjin728@gmail.com>
2026-03-22 01:13:57 +0800

    线程的优先级

---
+++ src/ThreadMethod2/Demo01.java
@@ -0,0 +1,27 @@
+package ThreadMethod2;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        /**
+         * setPriority(int newPriority)    设置线程的优先级
+         * final int getPriority()         获取线程的优先级
+         */
+
+        //创建线程要执行的参数对象
+        MyRunnable mr=new MyRunnable();
+
+        //创建线程对象
+        Thread t1=new Thread(mr,"飞机");
+        Thread t2=new Thread(mr,"坦克");
+
+        t1.setPriority(1);
+        t2.setPriority(10);
+        t1.start();
+        t2.start();
+
+        /**
+         * 优先级不是绝对的
+         * 是概率问题
+         */
+    }
+}
