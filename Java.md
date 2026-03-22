df260cbb10777c00f8950a59e76ff6d54f812507
menjinyan <menjin728@gmail.com>
2026-03-22 16:39:05 +0800

    同步代码块

--- Java.md
+++ Java.md
@@ -270,4 +270,7 @@ Stream 接口中静态方法 of 的细节
 ![img.png](document.image/多线程的实现方式.png)  
 ### 多线程三种方式对比  
 ![img.png](document.image/多线程三种方式对比.png)  
-### 常见的成员方法  
+### 线程的生命周期  
+![img.png](document.image/线程的生命周期.png)  
+### 同步代码块  
+![img.png](document.image/同步代码块.png)
---
+++ src/ThreadSafe/MyThread.java
@@ -0,0 +1,31 @@
+package ThreadSafe;
+
+public class MyThread extends Thread {
+
+    //表示这个类的所有对象，都共享ticket数据
+    static int tick=0;
+
+    //锁对象要是唯一的
+    static Object obj=new Object();
+
+    @Override
+    public void run() {
+        while (true) {
+
+            //同步代码块
+            synchronized (obj){
+                if(tick < 100) {
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    tick++;
+                    System.out.println(getName()+"正在卖"+tick + "买票");
+                }else  {
+                    break;
+                }
+            }
+        }
+    }
+}
