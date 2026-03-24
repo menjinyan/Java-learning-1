25f1b623d01a9147bc9b72785f02b3dac760d7b4
menjinyan <menjin728@gmail.com>
2026-03-25 00:02:16 +0800

    UDP协议小练习

---
+++ src/UDP/Demo03.java
@@ -0,0 +1,38 @@
+package UDP;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class Demo03 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * 按照下面的要求实现程序
+         * UDP 发送数据：数据来自于键盘录入，直到输入的数据是 886，发送数据结束
+         * UDP 接收数据：因为接收端不知道发送端什么时候停止发送，故采用死循环接收
+         */
+
+        //1、创建DatagramSocket对象
+        DatagramSocket ds=new DatagramSocket();
+
+        //2、打包数据
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入数据");
+        while (true) {
+            String str=sc.nextLine();
+            if("886".equals(str)){
+                break;
+            }
+            byte[] bytes = str.getBytes();
+            InetAddress address = InetAddress.getByName("127.0.0.1");
+            int port = 10086;
+            DatagramPacket dp=new DatagramPacket(bytes,bytes.length,address,port);
+
+            //3、发送数据
+            ds.send(dp);
+        }
+
+        //4、释放资源
+        ds.close();
+    }
+}
